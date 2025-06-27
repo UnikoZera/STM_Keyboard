4d55fb19db5560be9f8dada0b1814b9bacfcd4b8
UnikoZera <3864437775@qq.com>
2025-06-28 01:18:32 +0800

    first commit

---
+++ Core/Inc/main.h
@@ -0,0 +1,99 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define SysWork_Light_Pin GPIO_PIN_13
+#define SysWork_Light_GPIO_Port GPIOC
+#define Key3_G_Pin GPIO_PIN_4
+#define Key3_G_GPIO_Port GPIOA
+#define Key2_R_Pin GPIO_PIN_5
+#define Key2_R_GPIO_Port GPIOA
+#define Key3_R_Pin GPIO_PIN_6
+#define Key3_R_GPIO_Port GPIOA
+#define Key3_B_Pin GPIO_PIN_0
+#define Key3_B_GPIO_Port GPIOB
+#define Key2_B_Pin GPIO_PIN_10
+#define Key2_B_GPIO_Port GPIOB
+#define Key1_R_Pin GPIO_PIN_8
+#define Key1_R_GPIO_Port GPIOA
+#define Key1_G_Pin GPIO_PIN_9
+#define Key1_G_GPIO_Port GPIOA
+#define Key1_B_Pin GPIO_PIN_10
+#define Key1_B_GPIO_Port GPIOA
+#define OLED_SCL_Pin GPIO_PIN_15
+#define OLED_SCL_GPIO_Port GPIOA
+#define Key2_G_Pin GPIO_PIN_3
+#define Key2_G_GPIO_Port GPIOB
+#define Key4_R_Pin GPIO_PIN_6
+#define Key4_R_GPIO_Port GPIOB
+#define Key4_G_Pin GPIO_PIN_7
+#define Key4_G_GPIO_Port GPIOB
+#define Key4_B_Pin GPIO_PIN_8
+#define Key4_B_GPIO_Port GPIOB
+#define OLED_SDA_Pin GPIO_PIN_9
+#define OLED_SDA_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
